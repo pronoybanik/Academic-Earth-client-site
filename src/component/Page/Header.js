9be365cbad91b7f1f6bf83c3b035ca3639998da0
PRONOY <pronoybanik82@gmail.com>
2022-10-26 00:34:31 +0600

    site naveber add

--- src/component/Page/Header.js
+++ src/component/Page/Header.js
@@ -3,7 +3,6 @@ import { Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
@@ -40,6 +39,14 @@ const Header = () => {
             </NavDropdown> */}
             
           </Nav>
+          <div>
+            <Link to='/login'>
+            <Button className='btn btn-light'>LogIn</Button>
+            </Link>
+            <Link to='register'> 
+            <Button className='btn btn-light'>Register</Button>
+            </Link>
+          </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
--- src/component/Page/LeftSideNav/LeftSideNav.js
+++ src/component/Page/LeftSideNav/LeftSideNav.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import NewSummaryNav from '../Info/NewSummaryNav';
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([]);
+
+
+    useEffect(() => {
+        fetch('http://localhost:5000/subject-categories')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
     return (
         <div>
-            <h2>LestSideNav page</h2>
+            <h2>LestSideNav page: {categories.length}</h2>
+            {
+                categories.map(category => <NewSummaryNav
+                key={category}
+                category={category}
+                
+                ></NewSummaryNav>)
+            }
         </div>
     );
 };
