ee290cfc6fd3426bbf433b28ed7572ccf848dfc8
PRONOY <pronoybanik82@gmail.com>
2022-10-27 05:44:15 +0600

    privaterouter oky  page complite

--- src/component/Router/PrivateRouter.js
+++ src/component/Router/PrivateRouter.js
@@ -1,20 +1,20 @@
 import React, { useContext } from 'react';
-import { Navigate,  useLocation } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from './AuthProvider';
 
-const PrivateRouter = ({children}) => {
-    const { user, loader} = useContext(AuthContext);
+const PrivateRouter = ({ children }) => {
+    const { user, loader } = useContext(AuthContext);
     console.log(user, loader);
     const location = useLocation();
 
-    if(loader){
+    if (loader) {
         return <div>Loading ...</div>
     }
 
-    if(!user){
-        return <Navigate to='/login' state={{from: location}} replace></Navigate>
-    } 
-    return children
+    if (!user) {
+        return <Navigate to='/login' state={{ from: location }} replace></Navigate>
+    }
+    return children;
 };
 
 export default PrivateRouter;
